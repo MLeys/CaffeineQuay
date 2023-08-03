b415bf833857ed4e38787709264162e1ad554aea
MLeys <102929892+MLeys@users.noreply.github.com>
2023-08-03 16:41:49 -0400

    EDIT: ingredient model

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+import { ingredientClasses } from "../src/lists/ingredientClasses";
+
 const ingredientSchema = new Schema({
   name: {type: String, required: true},
-  class: {type: String, required: true}, // future list of options
+  class: {type: String, required: true, enum: ingredientClasses},
   unitOfMeasurement: {type: String, required: true},
   servingSize: {type: Number, required: true, default: 1},
   packageSize: {type: Number, required: true, default: 1},
   packageCost: {type: Number, required: true, default: 0},
-  costPerUnit: {type: Number, required: true, default: (servingSize / packageSize)* packageCost},
+  costPerUnit: {type: Number, required: true, default: 0},
 });
 
 export default mongoose.model('Ingredient', ingredientSchema);
